0453d9a26b13e35927c22bee02efb0a77944aeb2
DehaEstelle <estelaroldeha@gmail.com>
2023-02-16 16:39:51 +0100

    4545454554545454545

--- ReactJS/data_ex1.js
+++
@@ -1,8 +0,0 @@
-
-    const MESSAGES = [
-        { message : "React JS" },
-        { message : "React Native" },
-        { message : "Angular" },
-        { message : "Symfony" },
-        { message : "MongoDB" },
-    ];
